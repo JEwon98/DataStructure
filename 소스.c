3858de84c28f4fc2bc0647e9365bc9427d88e608
JEwon98 <33649813+JEwon98@users.noreply.github.com>
2018-04-15 23:24:59 +0900

    PostFixCal.c
    끝

--- 소스.c
+++ 소스.c
@@ -1,19 +1,24 @@
 #pragma warning (disable :4996)
-
+#define MAX_STACK_SIZE 100
+#define $ eos
 #include <stdio.h>
 #include <stdlib.h>
-typedef enum {lparen, rparen, plus, minus, times, divide, mod, eos, operand} precedence;
+#include <string.h>
+typedef enum { lparen, rparen, plus, minus, times, divide, mod, eos, operand } precedence;
 char expr[100];
 int top = -1;
-typedef struct {
-	char x;
 
-}Stack;
-
-Stack stack[100];
+int stack[100];
+int eval(void);
+precedence getToken(char *symbol, int *n);
+void push(char a);
+void stackFull();
+char pop();
+char stackEmpty();
 
 int main() {
-	scanf("%s", expr);
+	gets(expr);
+	printf("%d\n", eval());
 }
 int eval(void) {
 	precedence token;
@@ -26,12 +31,32 @@ int eval(void) {
 	while (token != eos) {
 		if (token == operand)
 			push(symbol - '0');
+		else {
+			op2 = pop();
+			op1 = pop();
 
+			switch (token) {
+			case plus: push(op1 + op2);
+				break;
+			case minus: push(op1 - op2);
+				break;
+			case times: push(op1*op2);
+				break;
+			case divide: push(op1 / op2);
+				break;
+			case mod: push(op1%op2);
+
+			}
+
+		}
+		token = getToken(&symbol, &n);
 	}
-
-
+	return pop();
 }
 precedence getToken(char *symbol, int *n) {
+	if (*n >= strlen(expr))
+		return $;
+
 	*symbol = expr[(*n)++];
 	switch (*symbol) {
 	case '(': return lparen;
@@ -44,16 +69,23 @@ precedence getToken(char *symbol, int *n) {
 	default: return operand;
 
 	}
-	void push(char a) {
-		
-		if (top >= MAX_STACK_SIZE - 1)
-			stackFull();
+}
+void push(char a) {
+	if (top >= MAX_STACK_SIZE - 1)
+		stackFull();
 
-		stack[++top] = a;
-	}
-	void stackFull() {
-		fprintf(stderr, "Stack is full\n");
-		exit(EXIT_FAILURE);
-	}
-
+	stack[++top] = a;
+}
+void stackFull() {
+	fprintf(stderr, "Stack is full\n");
+	exit(EXIT_FAILURE);
+}
+char pop() {
+	if (top == -1)
+		return stackEmpty();
+	return stack[top--];
+}
+char stackEmpty() {
+	fprintf(stderr, "Stack is empty\n");
+	exit(EXIT_FAILURE);
 }
