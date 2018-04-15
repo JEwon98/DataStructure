c844dd58385720273a42c231df5c9df47b15a30e
JEwon98 <33649813+JEwon98@users.noreply.github.com>
2018-04-15 22:44:50 +0900

    MAze.c

--- Maze.c
+++ Maze.c
@@ -1,40 +1,41 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-#define EXIT_ROW 5
-#define EXIT_COL 5
+#define EXIT_ROW 4
+#define EXIT_COL 4
 #define MAX_STACK_SIZE 100
 #define TRUE 1
 #define FALSE 0
-#define EXIT_FAILURE 0
 
-
-typedef struct{
+typedef struct {
 	short int row;
 	short int col;
 	short int dir;
 }element;
 
-typedef struct{
+typedef struct {
 	short int vert;
 	short int horiz;
 } offsets;
 
-element stack[36];
+element stack[100];
 int top = 0;
 element pop();
 void push(element item);
 element stackEmpty();
 void stackFull();
+int mark[6][6] = { 0 };
 
-int main(){
-	int maze[6][6] = {	(1, 1, 1, 1, 1, 1),
-						(1, 0, 0, 1, 1, 1),
-						(1, 1, 1, 0, 1, 1),
-						(1, 0, 0, 1, 0, 1),
-						(1, 1, 1, 0, 0, 1),
-						(1, 1, 1, 1, 1, 1) };
-	int mark[6][6] = { 0 };
 
+int main() {
+	int maze[6][6] = {
+		{ 1, 1, 1, 1, 1, 1 },
+		{ 1, 0, 0, 1, 1, 1 },
+		{ 1, 1, 1, 0, 1, 1 },
+		{ 1, 0, 0, 1, 0, 1 },
+		{ 1, 1, 1, 0, 0, 1 },
+		{ 1, 1, 1, 1, 1, 1 }
+	};
 	offsets move[8];
 	move[0].vert = -1; move[0].horiz = 0;
 	move[1].vert = -1; move[1].horiz = 1;
@@ -45,19 +46,19 @@ int main(){
 	move[6].vert = 0; move[6].horiz = -1;
 	move[7].vert = -1; move[7].horiz = -1;
 
-	int i, top, row, col, nextRow, nextCol, dir, found = FALSE;
+	int i, row, col, nextRow, nextCol, dir, found = FALSE;
 	element position;
 	mark[1][1] = 1; top = 0;
 	stack[0].row = 1; stack[0].col = 1; stack[0].dir = 1;
-	while (!found && top > -1){
-		position = pop(&top);
+	while (!found && top > -1) {
+		position = pop();
 		row = position.row; col = position.col; dir = position.dir;
-		while (!found && dir < 8){
+		while (!found && dir < 8) {
 			nextRow = row + move[dir].vert;
 			nextCol = col + move[dir].horiz;
 			if (nextRow == EXIT_ROW && nextCol == EXIT_COL)
 				found = TRUE;
-			else if (!maze[nextRow][nextCol] && !mark[nextRow][nextCol]){
+			else if (!maze[nextRow][nextCol] && !mark[nextRow][nextCol]) {
 				mark[nextRow][nextCol] = 1;
 				position.row = row; position.col = col; position.dir = ++dir;
 				push(position);
@@ -66,31 +67,31 @@ int main(){
 			else ++dir;
 		}
 	}
-	if (found){
+	if (found) {
 		printf("The path is:\n");
 		printf("row  col\n");
 		for (i = 0; i <= top; i++)
-			printf("%2d%5d", stack[i].row, stack[i].col);
-		printf("%2d%5d", row, col);
-		printf("%2d%5d", EXIT_ROW, EXIT_COL);
+			printf("%2d%5d\n", stack[i].row, stack[i].col);
+		printf("%2d%5d\n", row, col);
+		printf("%2d%5d\n", EXIT_ROW, EXIT_COL);
 	}
 	else printf("The maze does not have a path\n");
 }
-void push(element item){
+void push(element item) {
 	if (top >= MAX_STACK_SIZE - 1)
 		stackFull();
 	stack[++top] = item;
 }
-element pop(){
+element pop() {
 	if (top == -1)
 		return stackEmpty();
 	return stack[top--];
 }
-element stackEmpty(){
-	fprintf(stderr,"Stack is empty");
+element stackEmpty() {
+	fprintf(stderr, "Stack is empty\n");
 	exit(EXIT_FAILURE);
 }
 void stackFull() {
-	fprintf(stderr, "Stack is full");
+	fprintf(stderr, "Stack is full\n");
 	exit(EXIT_FAILURE);
 }
